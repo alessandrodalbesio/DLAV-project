d6a56c6d9d8b0c1c8ded1ffd9628503c7bc24ec0
Alessandro Dalbesio <alessandro.dalbesio@epfl.ch>
2024-05-05 21:23:14 +0200

    Updated scheduler

--- SCITAS/scheduler.sh
+++ SCITAS/scheduler.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Set the username
-username="dalbesio"
+# Define some variables
+experiments_folder_name="experiments"
+name_method_file="ptr"
+name_config_file="config"
+
+# Get the username
+username=whoami 
+echo "Running the scheduler script as user: $username"
 
 # Activate the virtual environment
 source /home/$username/DLAV-project/.venv/bin/activate
 
 # Set the directory path
-sched_config="/home/$username/DLAV-project/SCITAS/sched_config"
+sched_config="/home/$username/DLAV-project/SCITAS/$experiments_folder_name"
 dest_dir="/home/$username/DLAV-project/motionnet/configs"
+
+# Run the test generator script
+python /home/$username/DLAV-project/SCITAS/test_generator.py --experiments_folder_name $experiments_folder_name --name_method_file $name_method_file --name_config_file $name_config_file
 
 # Move to the main directory
 cd /home/$username/DLAV-project/motionnet
@@ -19,12 +28,13 @@ if [ -d "$sched_config" ]; then
             echo "Running experiment in directory: $exp_directory"
 
             # Copy the config files to the main directory
-            cp "$exp_directory/config.yaml" "$dest_dir/config.yaml"
-            cp "$exp_directory/ptr.yaml" "$dest_dir/method/ptr.yaml"
+            cp "$exp_directory/$name_config_file.yaml" "$dest_dir/$name_config_file.yaml"
+            cp "$exp_directory/$name_method_file.yaml" "$dest_dir/method/$name_method_file.yaml"
 
             echo "Copied config files to the main directory"
+
             # Run the experiment
-            python train.py method=ptr
+            # python train.py method=ptr
         fi
     done
 else
--- SCITAS/test_generator.py
+++ SCITAS/test_generator.py
@@ -1,29 +1,157 @@
+## Parameters definition ##
+data = {
+  "seed": [0],
+  "past_len": [21],
+  "future_len": [60],
+  "num_modes": [1,5],
+  "hidden_size": [64,128,256],
+  "num_encoder_layers": [1],
+  "num_decoder_layers": [1],
+  "tx_hidden_size": [1],
+  "tx_num_heads": [1],
+  "dropout": [0.1],
+  "max_epochs": [100],
+  "learning_rate": [0.0005],
+  "learning_rate_scheduler": [[15,25,45,55,65,75]],
+  "train_batch_size": [256]
+}
+
+########################################################################################
+
+## Import the libraries ##
 import os
+import json
+import numpy as np
+import argparse
 
-## User definition ##
-user = "dalbesio"
 
-## Parameter definition ##
-lr_list = [0.0005]
-train_batch_list = [256]
-lr_scheduler_list = [[15,25,45,55,65,75]]
-dest_folder = f"/home/{user}/DLAV-project/sched_config"
-if os.path.exists(dest_folder):
-    os.system(f"rm -r {dest_folder}")
+def main(experiments_folder_name, config_name, method_name):
+    ## Parameter definition ##
 
-## Files definition ##
-index = 0
-os.makedirs(dest_folder, exist_ok=True)
-for lr in lr_list:
-    for train_size in train_batch_list:
-        for lr_sched in lr_scheduler_list:
-            name = f'{lr},{lr_sched},{train_size}'
+    # Get the username by the system (Linux or Windows)
+    try:
+        user = os.environ['USER']
+    except KeyError:
+        user = os.environ['USERNAME']
 
-            config = f"""
+    # Load the parameters
+    save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), experiments_folder_name)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # Define the indices
+    indeces = []
+    keys = list(data.keys())
+    for key in keys:
+        indeces.append([i for i in range(len(data[key]))])
+
+    # Define all the combinations
+    exp_num = 0
+    for i in cartesian(indeces):
+        data_exp = {}
+        for k in range(len(keys)):
+            data_exp[keys[k]] = data[keys[k]][i[k]]    
+
+        # Get the configuration files
+        config, ptr = get_config_files(user, f"{user}_exp_{i}", data_exp)
+
+        # Create the directory
+        dir_name = f"exp_{exp_num}"
+        exp_num += 1
+        dir_path = os.path.join(save_path, dir_name)
+        os.makedirs(dir_path, exist_ok=True)
+
+        # Save the configuration file
+        with open(os.path.join(dir_path, f"{config_name}.yaml"), "w") as f:
+            f.write(config)
+        print(f"Configuration file saved in {dir_path} as {config_name}.yaml")
+
+        # Save the PTR method file
+        with open(os.path.join(dir_path, f"{method_name}.yaml"), "w") as f:
+            f.write(ptr)
+        print(f"PTR method file saved in {dir_path} as {method_name}.yaml")
+
+def cartesian(arrays, out=None):
+    """
+    Credits: This function has been taken from https://stackoverflow.com/a/1235363
+    Generate a Cartesian product of input arrays.
+
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the Cartesian product of.
+    out : ndarray
+        Array to place the Cartesian product in.
+
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing Cartesian products
+        formed of input arrays.
+
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    #m = n / arrays[0].size
+    m = int(n / arrays[0].size)
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m, 1:])
+        for j in range(1, arrays[0].size):
+        #for j in xrange(1, arrays[0].size):
+            out[j*m:(j+1)*m, 1:] = out[0:m, 1:]
+    return out
+
+def get_config_files(user,name,data):
+
+    # Check if all keys are present
+    dir_key_needed = [
+        'past_len',
+        'future_len',
+        'num_modes',
+        'hidden_size',
+        'num_encoder_layers',
+        'num_decoder_layers',
+        'tx_hidden_size',
+        'tx_num_heads',
+        'dropout',
+        'max_epochs',
+        'learning_rate',
+        'learning_rate_scheduler',
+        'train_batch_size'
+    ]
+    for key in dir_key_needed:
+        if key not in data:
+            raise ValueError(f"Key {key} not found in data")
+
+    # Define the config file
+    config = f"""
 # exp setting
 exp_name: '{name}'
 ckpt_path: Null
-seed: 37
+seed: {data['seed']}
 debug: False
 devices: [0]
 
@@ -32,8 +160,8 @@ load_num_workers: 0
 train_data_path: ['/home/{user}/DLAV-project/.datasets/train']
 val_data_path: ['/home/{user}/DLAV-project/.datasets/val']
 max_data_num: [1000000]
-past_len: 21 # 0.1s
-future_len: 60 # 0.1s
+past_len: '{data['past_len']}' # 2.1 s
+future_len: '{data['future_len']}' # 6 s
 object_type: ['VEHICLE'] #, 'PEDESTRIAN', 'CYCLIST']
 line_type: ['lane','stop_sign','road_edge','road_line','crosswalk','speed_bump'] #['lane','stop_sign','road_edge','road_line','crosswalk','speed_bump']
 masked_attributes: ['z_axis, size'] # 'z_axis, size, velocity, acceleration, heading'
@@ -53,28 +181,28 @@ defaults:
 - method: ptr
             """
 
-            ptr = f"""
+    ptr = f"""
 # common
 model_name: ptr
-num_modes: 6
-hidden_size: 128
-num_encoder_layers: 2
-num_decoder_layers: 2
-tx_hidden_size: 384
-tx_num_heads: 16
-dropout: 0.1
+num_modes: {data['num_modes']}
+hidden_size: {data['hidden_size']}
+num_encoder_layers: {data['num_encoder_layers']}
+num_decoder_layers: {data['num_decoder_layers']}
+tx_hidden_size: {data['tx_hidden_size']}
+tx_num_heads: {data['tx_num_heads']}
+dropout: {data['dropout']}
 entropy_weight: 40.0
 kl_weight: 20.0
 use_FDEADE_aux_loss: True
 
 # train
-max_epochs: 80
-learning_rate: {lr}
-learning_rate_sched: {lr_sched}
+max_epochs: {data['max_epochs']}
+learning_rate: {data['learning_rate']}
+learning_rate_sched: {data['learning_rate_scheduler']}
 optimizer: Adam 
 scheduler: multistep 
 ewc_lambda: 2000
-train_batch_size: {train_size}
+train_batch_size: {data['train_batch_size']}
 eval_batch_size: 256
 grad_clip_norm: 5
 
@@ -88,21 +216,12 @@ point_sampled_interval: 1
 num_points_each_polyline: 20
 vector_break_dist_thresh: 1.0
             """
+    return config, ptr
 
-            # Create a folder for the experiment
-            folder = f"{dest_folder}/exp_{index}"
-            os.makedirs(folder, exist_ok=True)
-            index += 1
-
-            # Write the config file
-            with open(f"{folder}/config.yaml", "w") as f:
-                # Remove the first line break
-                config = config[1:]
-                # Write the config file
-                f.write(config)
-
-            with open(f"{folder}/ptr.yaml", "w") as f:
-                # Remove the first line break
-                ptr = ptr[1:]
-                # Write the config file
-                f.write(ptr)
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--experiments_folder_name", type=str, default=None)
+    args.add_argument("--dst_file_config", type=str, default=None)
+    args.add_argument("--dst_file_ptr", type=str, default=None)    
+    args = args.parse_args()
+    main(args.experiments_folder_name, args.dst_file_config, args.dst_file_ptr)
