14f21e9b6475cedd698c8ab682126224473e0802
elisaaferraraa <147555982+elisaaferraraa@users.noreply.github.com>
2024-05-23 16:18:14 +0200

    Comments on the decoder

--- motionnet/models/qcnet/modules/qcnet_decoder.py
+++ motionnet/models/qcnet/modules/qcnet_decoder.py
@@ -75,54 +75,81 @@ class QCNetDecoder(nn.Module):
         input_dim_r_pl2m = 3
         input_dim_r_a2m = 3
 
+        #LEGEND:
+        # a = neighbor agents
+        # m = mode query
+        # t = target agent
+
+        # DEFINITION OF EMBEDDINGS
+        # mode embedding: needed for mode queries ( categorical data )
         self.mode_emb = nn.Embedding(num_modes, hidden_dim)
-        self.r_t2m_emb = FourierEmbedding(input_dim=input_dim_r_t, hidden_dim=hidden_dim, num_freq_bands=num_freq_bands)
-        self.r_pl2m_emb = FourierEmbedding(input_dim=input_dim_r_pl2m, hidden_dim=hidden_dim,
+
+        # Fourier embedding ( continuous data ): target agent, map elements, neighbor agents, output
+        self.r_t2m_emb = FourierEmbedding(input_dim=input_dim_r_t, hidden_dim=hidden_dim, num_freq_bands=num_freq_bands) # target agent to mode query
+        self.r_pl2m_emb = FourierEmbedding(input_dim=input_dim_r_pl2m, hidden_dim=hidden_dim,                            # map element to mode query                                      
                                            num_freq_bands=num_freq_bands)
-        self.r_a2m_emb = FourierEmbedding(input_dim=input_dim_r_a2m, hidden_dim=hidden_dim,
+        
+        self.r_a2m_emb = FourierEmbedding(input_dim=input_dim_r_a2m, hidden_dim=hidden_dim,                              # neighbor agent to mode query
                                           num_freq_bands=num_freq_bands)
-        self.y_emb = FourierEmbedding(input_dim=output_dim + output_head, hidden_dim=hidden_dim,
+        self.y_emb = FourierEmbedding(input_dim=output_dim + output_head, hidden_dim=hidden_dim,                         # output embedding                           
                                       num_freq_bands=num_freq_bands)
+        
+        # GRU layer for trajectory embedding
         self.traj_emb = nn.GRU(input_size=hidden_dim, hidden_size=hidden_dim, num_layers=1, bias=True,
                                batch_first=False, dropout=0.0, bidirectional=False)
         self.traj_emb_h0 = nn.Parameter(torch.zeros(1, hidden_dim))
-        self.t2m_propose_attn_layers = nn.ModuleList(
+
+        # DEFINITION OF ATTENTION LAYERS
+        # Traj Proposal: cross attention layers
+        self.t2m_propose_attn_layers = nn.ModuleList(                                                       # target agent to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
-        self.pl2m_propose_attn_layers = nn.ModuleList(
+        self.pl2m_propose_attn_layers = nn.ModuleList(                                                      # map element to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
-        self.a2m_propose_attn_layers = nn.ModuleList(
+        self.a2m_propose_attn_layers = nn.ModuleList(                                                       # neighbor agent to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
+        # Traj Proposal: self attention layer
         self.m2m_propose_attn_layer = AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim,
                                                      dropout=dropout, bipartite=False, has_pos_emb=False)
-        self.t2m_refine_attn_layers = nn.ModuleList(
+        
+        # Traj Refinement: cross attention layers
+        self.t2m_refine_attn_layers = nn.ModuleList(                                                        # target agent to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
-        self.pl2m_refine_attn_layers = nn.ModuleList(
+        self.pl2m_refine_attn_layers = nn.ModuleList(                                                       # map element to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
-        self.a2m_refine_attn_layers = nn.ModuleList(
+        self.a2m_refine_attn_layers = nn.ModuleList(                                                        # neighbor agent to mode query
             [AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim, dropout=dropout,
                             bipartite=True, has_pos_emb=True) for _ in range(num_layers)]
         )
+        # Traj Refinement: self attention layer
         self.m2m_refine_attn_layer = AttentionLayer(hidden_dim=hidden_dim, num_heads=num_heads, head_dim=head_dim,
                                                     dropout=dropout, bipartite=False, has_pos_emb=False)
+        
+        # MLP LAYERS: i-th agent trajectory is parametrized  as a mixture of Laplacian distrib. in which the mixed component
+        # density at time step t is parametrized by:
+        # - loc_propose_pos: location of agent (Lpropose mixture component)
         self.to_loc_propose_pos = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
                                            output_dim=num_future_steps * output_dim // num_recurrent_steps)
+        # - scale_propose_pos: scale of agent (Lpropose mixture component)
         self.to_scale_propose_pos = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
                                              output_dim=num_future_steps * output_dim // num_recurrent_steps)
+        # - loc_propose_head: location of agent (Lrefine mixture component)  
         self.to_loc_refine_pos = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
                                           output_dim=num_future_steps * output_dim)
+        # - scale_propose_head: scale of agent (Lrefine mixture component)
         self.to_scale_refine_pos = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
                                             output_dim=num_future_steps * output_dim)
         if output_head:
+            # loc stands for location, conc stands for concentration
             self.to_loc_propose_head = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
                                                 output_dim=num_future_steps // num_recurrent_steps)
             self.to_conc_propose_head = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim,
@@ -135,40 +162,49 @@ class QCNetDecoder(nn.Module):
             self.to_conc_propose_head = None
             self.to_loc_refine_head = None
             self.to_conc_refine_head = None
+
+        # Lcls for mode classification
         self.to_pi = MLPLayer(input_dim=hidden_dim, hidden_dim=hidden_dim, output_dim=1)
         self.apply(weight_init)
 
     def forward(self,
                 data: HeteroData,
                 scene_enc: Mapping[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # INPUT DATA
         pos_m = data['agent']['position'][:, self.num_historical_steps - 1, :self.input_dim]
         head_m = data['agent']['heading'][:, self.num_historical_steps - 1].squeeze()
         head_vector_m = torch.stack([head_m.cos(), head_m.sin()], dim=-1).squeeze()
 
-        x_t = scene_enc['x_a'].reshape(-1, self.hidden_dim)
-        x_pl = scene_enc['x_pl'][:, self.num_historical_steps - 1].repeat(self.num_modes, 1)
-        x_a = scene_enc['x_a'][:, -1].repeat(self.num_modes, 1)
+        # ENCODING 
+        # Update mode queries with multiple contexts: target agent (1), history, map elements(2) and neighbors elements(3)
+        x_t = scene_enc['x_a'].reshape(-1, self.hidden_dim)                                     
+        x_pl = scene_enc['x_pl'][:, self.num_historical_steps - 1].repeat(self.num_modes, 1)    
+        x_a = scene_enc['x_a'][:, -1].repeat(self.num_modes, 1)                                 
         m = self.mode_emb.weight.repeat(scene_enc['x_a'].size(0), 1)
 
-        mask_src = data['agent']['valid_mask'][:, :self.num_historical_steps].contiguous()
+        # Mask for valid elements
+        mask_src = data['agent']['valid_mask'][:, :self.num_historical_steps].contiguous()           # src stands for source
         mask_src[:, :self.num_historical_steps - self.num_t2m_steps] = False
-        mask_dst = data['agent']['predict_mask'].any(dim=-1, keepdim=True).repeat(1, self.num_modes)
+        mask_dst = data['agent']['predict_mask'].any(dim=-1, keepdim=True).repeat(1, self.num_modes) # dst stands for destination
 
-        pos_t = data['agent']['position'][:, :self.num_historical_steps, :self.input_dim].reshape(-1, self.input_dim)
+        # Targer agent embedding (1)
+        pos_t = data['agent']['position'][:, :self.num_historical_steps, :self.input_dim].reshape(-1, self.input_dim) #--prepare for target agent to mode query--
         head_t = data['agent']['heading'][:, :self.num_historical_steps].reshape(-1).squeeze()
-        edge_index_t2m = bipartite_dense_to_sparse(mask_src.unsqueeze(2) & mask_dst[:, -1:].unsqueeze(1))
-        rel_pos_t2m = pos_t[edge_index_t2m[0]] - pos_m[edge_index_t2m[1]]
-        rel_head_t2m = wrap_angle(head_t[edge_index_t2m[0]] - head_m[edge_index_t2m[1]])
-        r_t2m = torch.stack(
+        edge_index_t2m = bipartite_dense_to_sparse(mask_src.unsqueeze(2) & mask_dst[:, -1:].unsqueeze(1))             
+        rel_pos_t2m = pos_t[edge_index_t2m[0]] - pos_m[edge_index_t2m[1]]                                             # relative position
+        rel_head_t2m = wrap_angle(head_t[edge_index_t2m[0]] - head_m[edge_index_t2m[1]])                              # relative heading
+        r_t2m = torch.stack(                                                                                          
             [torch.norm(rel_pos_t2m[:, :2], p=2, dim=-1),
              angle_between_2d_vectors(ctr_vector=head_vector_m[edge_index_t2m[1]].squeeze(), nbr_vector=rel_pos_t2m[:, :2].squeeze()),
              rel_head_t2m,
              (edge_index_t2m[0] % self.num_historical_steps) - self.num_historical_steps + 1], dim=-1)
-        r_t2m = self.r_t2m_emb(continuous_inputs=r_t2m, categorical_embs=None)
+        r_t2m = self.r_t2m_emb(continuous_inputs=r_t2m, categorical_embs=None)                                       # --continuous embedding--
         edge_index_t2m = bipartite_dense_to_sparse(mask_src.unsqueeze(2) & mask_dst.unsqueeze(1))
         r_t2m = r_t2m.repeat_interleave(repeats=self.num_modes, dim=0)
 
-        pos_pl = data['map_polygon']['position'][:, :self.input_dim]
+
+        # Map elements embedding (2)   
+        pos_pl = data['map_polygon']['position'][:, :self.input_dim]                                                #--prepare for map element to mode query--
         orient_pl = data['map_polygon']['orientation']
         edge_index_pl2m = radius(
             x=pos_m[:, :2],
@@ -180,15 +216,16 @@ class QCNetDecoder(nn.Module):
         edge_index_pl2m = edge_index_pl2m[:, mask_dst[edge_index_pl2m[1], 0]]
         rel_pos_pl2m = pos_pl[edge_index_pl2m[0]] - pos_m[edge_index_pl2m[1]]
         rel_orient_pl2m = wrap_angle(orient_pl[edge_index_pl2m[0]] - head_m[edge_index_pl2m[1]])
-        r_pl2m = torch.stack(
+        r_pl2m = torch.stack(                                                                                       # data prepared
             [torch.norm(rel_pos_pl2m[:, :2], p=2, dim=-1),
              angle_between_2d_vectors(ctr_vector=head_vector_m[edge_index_pl2m[1]], nbr_vector=rel_pos_pl2m[:, :2]),
              rel_orient_pl2m], dim=-1)
-        r_pl2m = self.r_pl2m_emb(continuous_inputs=r_pl2m, categorical_embs=None)
+        r_pl2m = self.r_pl2m_emb(continuous_inputs=r_pl2m, categorical_embs=None)                                  #--continuous embedding--
         edge_index_pl2m = torch.cat([edge_index_pl2m + i * edge_index_pl2m.new_tensor(
             [[data['map_polygon']['num_nodes']], [data['agent']['num_nodes']]]) for i in range(self.num_modes)], dim=1)
         r_pl2m = r_pl2m.repeat(self.num_modes, 1)
 
+        # Neighbor elements embedding (3)
         edge_index_a2m = radius_graph(
             x=pos_m[:, :2],
             r=self.a2m_radius,
@@ -198,11 +235,11 @@ class QCNetDecoder(nn.Module):
         edge_index_a2m = edge_index_a2m[:, mask_src[:, -1][edge_index_a2m[0]] & mask_dst[edge_index_a2m[1], 0]]
         rel_pos_a2m = pos_m[edge_index_a2m[0]] - pos_m[edge_index_a2m[1]]
         rel_head_a2m = wrap_angle(head_m[edge_index_a2m[0]] - head_m[edge_index_a2m[1]])
-        r_a2m = torch.stack(
+        r_a2m = torch.stack(                                                                                     # data prepared
             [torch.norm(rel_pos_a2m[:, :2], p=2, dim=-1),
              angle_between_2d_vectors(ctr_vector=head_vector_m[edge_index_a2m[1]], nbr_vector=rel_pos_a2m[:, :2]),
              rel_head_a2m], dim=-1)
-        r_a2m = self.r_a2m_emb(continuous_inputs=r_a2m, categorical_embs=None)
+        r_a2m = self.r_a2m_emb(continuous_inputs=r_a2m, categorical_embs=None)                                  #--continuous embedding--
         edge_index_a2m = torch.cat(
             [edge_index_a2m + i * edge_index_a2m.new_tensor([data['agent']['num_nodes']]) for i in
              range(self.num_modes)], dim=1)
@@ -210,11 +247,15 @@ class QCNetDecoder(nn.Module):
 
         edge_index_m2m = dense_to_sparse(mask_dst.unsqueeze(2) & mask_dst.unsqueeze(1))[0]
 
+        # DECODING TRAJECTORY PROPOSAL: anchor free
+        # initialize the proposal distribution: locs_pos is the location of the agent, scales_pos is the scale of the agent, locs_head is the agent's direction, concs_head is the concentration (see """variance""") of direction
         locs_propose_pos: List[Optional[torch.Tensor]] = [None] * self.num_recurrent_steps
         scales_propose_pos: List[Optional[torch.Tensor]] = [None] * self.num_recurrent_steps
         locs_propose_head: List[Optional[torch.Tensor]] = [None] * self.num_recurrent_steps
         concs_propose_head: List[Optional[torch.Tensor]] = [None] * self.num_recurrent_steps
+    
         for t in range(self.num_recurrent_steps):
+            # apply cross attention layers
             for i in range(self.num_layers):
                 m = m.reshape(-1, self.hidden_dim)
                 m = self.t2m_propose_attn_layers[i]((x_t, m), r_t2m, edge_index_t2m)
@@ -222,13 +263,17 @@ class QCNetDecoder(nn.Module):
                 m = self.pl2m_propose_attn_layers[i]((x_pl, m), r_pl2m, edge_index_pl2m)
                 m = self.a2m_propose_attn_layers[i]((x_a, m), r_a2m, edge_index_a2m)
                 m = m.reshape(self.num_modes, -1, self.hidden_dim).transpose(0, 1).reshape(-1, self.hidden_dim)
+            # apply self attention layer
             m = self.m2m_propose_attn_layer(m, None, edge_index_m2m)
             m = m.reshape(-1, self.num_modes, self.hidden_dim)
+            # compute proposal distribution
             locs_propose_pos[t] = self.to_loc_propose_pos(m)
             scales_propose_pos[t] = self.to_scale_propose_pos(m)
             if self.output_head:
                 locs_propose_head[t] = self.to_loc_propose_head(m)
                 concs_propose_head[t] = self.to_conc_propose_head(m)
+
+        # compute the cumulative sum of the proposal distribution to get the final distribution
         loc_propose_pos = torch.cumsum(
             torch.cat(locs_propose_pos, dim=-1).view(-1, self.num_modes, self.num_future_steps, self.output_dim),
             dim=-2)
@@ -238,6 +283,7 @@ class QCNetDecoder(nn.Module):
                 alpha=1.0) +
             1.0,
             dim=-2) + 0.1
+        # compute the cumulative sum of the proposal distribution for the head
         if self.output_head:
             loc_propose_head = torch.cumsum(torch.tanh(torch.cat(locs_propose_head, dim=-1).unsqueeze(-1)) * math.pi,
                                             dim=-2)
@@ -253,12 +299,18 @@ class QCNetDecoder(nn.Module):
             m = self.y_emb(loc_propose_pos.detach().view(-1, self.output_dim))
         m = m.reshape(-1, self.num_future_steps, self.hidden_dim).transpose(0, 1)
         m = self.traj_emb(m, self.traj_emb_h0.unsqueeze(1).repeat(1, m.size(1), 1))[1].squeeze(0)
+        # m represents the possible future trajectory of the target agent, they are the anchor points for the next step
+
+
+        # DECODING TRAJECTORY REFINEMENT: anchor based
+        # apply cross attention layers
         for i in range(self.num_layers):
             m = self.t2m_refine_attn_layers[i]((x_t, m), r_t2m, edge_index_t2m)
             m = m.reshape(-1, self.num_modes, self.hidden_dim).transpose(0, 1).reshape(-1, self.hidden_dim)
             m = self.pl2m_refine_attn_layers[i]((x_pl, m), r_pl2m, edge_index_pl2m)
             m = self.a2m_refine_attn_layers[i]((x_a, m), r_a2m, edge_index_a2m)
             m = m.reshape(self.num_modes, -1, self.hidden_dim).transpose(0, 1).reshape(-1, self.hidden_dim)
+        # apply self attention layer
         m = self.m2m_refine_attn_layer(m, None, edge_index_m2m)
         m = m.reshape(-1, self.num_modes, self.hidden_dim)
         loc_refine_pos = self.to_loc_refine_pos(m).view(-1, self.num_modes, self.num_future_steps, self.output_dim)
@@ -275,6 +327,8 @@ class QCNetDecoder(nn.Module):
                                                         1))
             conc_refine_head = scale_refine_pos.new_zeros((scale_refine_pos.size(0), self.num_modes,
                                                            self.num_future_steps, 1))
+            
+        # MLP LAYER: get the probability of each mode
         pi = self.to_pi(m).squeeze(-1)
 
         return {
